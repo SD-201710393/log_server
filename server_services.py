8b0ec7a525366a0d72c2454eb26906c7e8f235aa
Ramon Darwich <velociraptorbaiano@yahoo.com.br>
2021-11-27 10:11:46 -0300

    Minor typos 2

--- server_services.py
+++ server_services.py
@@ -1,70 +1,55 @@
 known_servers = [{
-            "id": "201720295",
-            "nome": "Alanna",
-            "url": "https://sd-ascampos-20212.herokuapp.com/"
-        },
-        {
-            "id": "201512136",
-            "nome": "Annya",
-            "url": "https://sd-annyaourives-20212.herokuapp.com/"
-        },
-        {
-            "id": "201512137",
-            "nome": "Daniel",
-            "url": None
-        },
-        {
-            "id": "201710375",
-            "nome": "Emmanuel",
-            "url": "https://sd-emmanuel.herokuapp.com/"
-        },
-        {
-            "id": "201420373",
-            "nome": "Gabriel",
-            "url": None
-        },
-        {
-            "id": "201710376",
-            "nome": "Guilherme",
-            "url": "https://nodejs-sd-guilhermesenna.herokuapp.com/"
-        },
-        {
-            "id": "201710377",
-            "nome": "Thiago",
-            "url": "https://sd-api-uesc.herokuapp.com/"
-        },
-        {
-            "id": "201810665",
-            "nome": "Jenilson",
-            "url": "https://jenilsonramos-sd-20211.herokuapp.com/"
-        },
-        {
-            "id": "201610327",
-            "nome": "Joao",
-            "url": "https://sd-joaopedrop-20212.herokuapp.com/"
-        },
-        {
-            "id": "201610337",
-            "nome": "Luis",
-            "url": "https://sd-20212-luiscarlos.herokuapp.com/"
-        },
-        {
-            "id": "201620181",
-            "nome": "matheus",
-            "url": None
-        },
-        {
-            "id": "201620400",
-            "nome": "Nassim",
-            "url": "https://sd-nassimrihan-2021-2.herokuapp.com/"
-        },
-        {
-            "id": "201710396",
-            "nome": "Robert",
-            "url": "https://pratica-sd.herokuapp.com/"
-        },
-        {
-            "id": "201720308",
-            "nome": "Victor",
-            "url": "https://sd-victor-20212.herokuapp.com/"
-        }]
+        "id": "201810665",
+        "nome": "Jenilson",
+        "url": "https://jenilsonramos-sd-20211.herokuapp.com/",
+    },
+    {
+        "id": "201720308",
+        "nome": "Victor",
+        "url": "https://sd-victor-20212.herokuapp.com/",
+    },
+    {
+        "id": "201720295",
+        "nome": "Allana",
+        "url": "https://sd-ascampos-20212.herokuapp.com/",
+    },
+    {
+        "id": "201710396",
+        "nome": "Robert",
+        "url": "https://pratica-sd.herokuapp.com/",
+    },
+    {
+        "id": "201710377",
+        "nome": "Hiago",
+        "url": "https://sd-api-uesc.herokuapp.com/",
+    },
+    {
+        "id": "201710376",
+        "nome": "Guilherme",
+        "url": "https://nodejs-sd-guilhermesenna.herokuapp.com/",
+    },
+    {
+        "id": "201710375",
+        "nome": "Emmanuel",
+        "url": "https://sd-emmanuel.herokuapp.com/",
+    },
+    {
+        "id": "201620400",
+        "nome": "Nassim",
+        "url": "https://sd-nassimrihan-2021-2.herokuapp.com/",
+    },
+    {
+        "id": "201610337",
+        "nome": "Luís",
+        "url": "https://sd-20212-luiscarlos.herokuapp.com/",
+    },
+    {
+        "id": "201610327",
+        "nome": "João",
+        "url": "https://sd-joaopedrop-20212.herokuapp.com/",
+    },
+    {
+        "id": "201512136",
+        "nome": "Annya",
+        "url": "https://sd-annyaourives-20212.herokuapp.com/",
+    }]
